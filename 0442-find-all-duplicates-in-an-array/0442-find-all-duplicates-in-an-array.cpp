02fbf0e024ad88bd971cd3c3dc3888044be1ca3e
Dhruv Agarwal <101520223+Dhruv385@users.noreply.github.com>
2023-02-03 21:20:45 +0530

    Time: 78 ms (60.63%), Space: 33.6 MB (57.42%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
@@ -6,7 +6,7 @@ public:
         for(int i=0;i<nums.size()-1;){
             if(nums[i]==nums[i+1]){
                 v.push_back(nums[i]);
-                i+=2;
+                i++;
             }
             else
                 i++;
