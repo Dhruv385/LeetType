a07f086beb57f647df16600f4894e7aded32fda0
Dhruv Agarwal <101520223+Dhruv385@users.noreply.github.com>
2023-02-04 23:46:27 +0530

    Time: 8 ms (38.25%), Space: 9.8 MB (32.25%) - LeetHub

---
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+        vector<int> v;
+        int i=0;
+        int j=n;
+        while(i<n && j<2*n){
+            v.push_back(nums[i]);
+            v.push_back(nums[j]);
+            i++;j++;
+        }
+        return v;
+    }
+};
