9c124f0c2074433bafc125e03400d30a72785924
Dhruv Agarwal <101520223+Dhruv385@users.noreply.github.com>
2023-03-06 21:05:11 +0530

    Time: 8 ms (73.64%), Space: 9.6 MB (68.05%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int cnt=0;
+        if(nums.size()%3!=0){
+            while(nums.size()%3!=0)
+                nums.push_back(0);
+        }
+        for(int i=0;i<nums.size()-2;i+=3){
+            if(nums[i]!=nums[i+2])
+                return nums[i];
+        }
+        return 0;
+    }
+};
