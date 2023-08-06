fa276771903115b7fbb5a02b71ff6ca57b62c116
Dhruv Agarwal <101520223+Dhruv385@users.noreply.github.com>
2023-08-06 10:05:25 +0530

    Added solution - LeetHub

--- String Permutations - GFG/string-permutations.cpp
+++ String Permutations - GFG/string-permutations.cpp
@@ -4,26 +4,25 @@ using namespace std;
 
 // } Driver Code Ends
 class Solution{
-    private:
-    void solve(string S,vector<string> &v,int ind){
-        if(ind==S.length()){
-            v.push_back(S);
-            return ;
-        }
-        for(int j=ind;j<S.length();j++){
-            swap(S[j],S[ind]);
-            solve(S,v,ind+1);
-            swap(S[j],S[ind]);
-        }
-    }
     public:
     //Complete this function
-    
-    vector<string> permutation(string S)
-    {
+    int fact(int n){
+        int f=1;
+        while(n>0){
+            f=f*n;
+            n--;
+        }
+        return f;
+    }
+    vector<string> permutation(string s){
+        int n=s.size();
+        int x=fact(n);
         vector<string> v;
-        int ind=0;
-        solve(S,v,ind);
+        v.push_back(s);
+        for(int i=0;i<x-1;i++){
+            next_permutation(s.begin(),s.end());
+            v.push_back(s);
+        }
         sort(v.begin(),v.end());
         return v;
     }
